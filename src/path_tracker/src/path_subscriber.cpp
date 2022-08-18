39a06e0795325fec942bfe042657faeba500d0b9
Hasanzfdn <hsnzfdn@gmail.com>
2022-08-18 13:41:14 +0300

    Fixed code bug. Path message receive control

--- src/path_tracker/src/path_subscriber.cpp
+++ src/path_tracker/src/path_subscriber.cpp
@@ -24,12 +24,11 @@ public:
     PathSubscriber()
     : Node("tracker")
   {
-      this->declare_parameter("frequency", 100.0);
+      this->declare_parameter("frequency", 0.1);
       this->declare_parameter("car_length", 2.0);
       this->declare_parameter("car_width", 1.0);
       this->declare_parameter("path_topic", "/path_ellipse_partial");
       this->declare_parameter("publish_vehicle_topic", "/Vehicle_polygon");
-
 
       this->get_parameter("frequency", frequency);
       this->get_parameter("car_length", car_length);
@@ -40,6 +39,7 @@ public:
       RCLCPP_INFO(this->get_logger(), "----------------------------------------------------");
       RCLCPP_INFO(this->get_logger(), "frequency: %f", frequency);
       RCLCPP_INFO(this->get_logger(), "car_length: %f", car_length);
+      RCLCPP_INFO(this->get_logger(), "car_length: %f", car_length);
       RCLCPP_INFO(this->get_logger(), "car_width: %f", car_width);
       RCLCPP_INFO(this->get_logger(), "path_topic: %s", path_topic.c_str());
       RCLCPP_INFO(this->get_logger(), "publish_vehicle_topic: %s", publish_vehicle_topic.c_str());
@@ -49,7 +49,7 @@ public:
 
       publisher_ = this->create_publisher<geometry_msgs::msg::PolygonStamped>(publish_vehicle_topic, 10);
       subscription_ = this->create_subscription<nav_msgs::msg::Path>(path_topic,10, std::bind(&PathSubscriber::topic_callback, this, _1));
-      timer_ = this->create_wall_timer(500ms, std::bind(&PathSubscriber::create_polygon_points, this));
+      timer_ = this->create_wall_timer(frequency*1000ms, std::bind(&PathSubscriber::create_polygon_points, this));
 
 
       }
@@ -87,10 +87,14 @@ private:
         return temp_vehicle;
     }
 
-   void create_polygon_points (){
+   void create_polygon_points(){
         geometry_msgs::msg::Point32 point_;
 
        std::cout << "create polygon points " << std::endl;
+
+       if(path_message.empty()){
+           return;
+       }
 
         if (counter <= path_message.size()) {
             // Create polygon points according to base_link points.( Base_link almost center of the car).
@@ -117,17 +121,16 @@ private:
             vehicle.polygon.points.push_back(point_);
 
             counter++;
-            std::cout << "created vehicle." << std::endl;
+
         }
         else {
             counter = 0;
         }
 
-        vehicle =   rotate_point(path_message[counter],vehicle);
-        vehicle.header.frame_id = "map";
-        publisher_->publish(vehicle);
+       vehicle =   rotate_point(path_message[counter],vehicle);
+       vehicle.header.frame_id = "map";
+       publisher_->publish(vehicle);
        std::cout << "published vehicle." << std::endl;
-
     }
 
 
